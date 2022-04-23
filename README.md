7c96b04ad9983ce11379b6cb6db7f6ae45cb9c0c
Jizu Sun <jizu.sun@philips.com>
2022-04-23 03:28:48 +0000

    nss-myhostname

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # k3d-argocd
+
+- https://blog.differentpla.net/blog/2022/02/02/argocd/
+- https://traefik.io/blog/traefik-proxy-kubernetes-101/
+- NSS-myhostname: https://k3d.io/v5.2.0/usage/registries/#preface-referencing-local-registries
---
+++ start-k3d.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+name=my-cluster
+nodes=3
+registry_name=k3d-my-registry
+max="$(($nodes-1))"
+
+k3d cluster delete $name
+k3d registry delete $registry_name
+
+k3d cluster create $name -a $nodes \
+    -p 8080:80@loadbalancer \
+    -p "8443:443@loadbalancer" \
+    --api-port 6443 \
+    --registry-create $registry_name:0.0.0.0:5000 \
+    -e "HTTP_PROXY=$http_proxy@server:0" \
+    -e "HTTPS_PROXY=$http_proxy@server:0" \
+    -e "http_proxy=$http_proxy@server:0" \
+    -e "https_proxy=$http_proxy@server:0" \
+    -e "NO_PROXY=$registry_name,$no_proxy@server:0" \
+    -e "HTTP_PROXY=$http_proxy@agent:0-$max" \
+    -e "HTTPS_PROXY=$http_proxy@agent:0-$max" \
+    -e "http_proxy=$http_proxy@agent:0-$max" \
+    -e "https_proxy=$http_proxy@agent:0-$max" \
+    -e "NO_PROXY=$registry_name,$no_proxy@agent:0-$max" 
